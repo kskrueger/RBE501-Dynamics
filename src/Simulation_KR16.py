25de0f077c80a2cdb790c1b2f75dfa78d8ac5dc1
Kyle Mitchell <kgmitchell@wpi.edu>
2022-11-09 17:01:02 -0500

    Implemented some methods

--- src/Simulation_KR16.py
+++ src/Simulation_KR16.py
@@ -1,10 +1,11 @@
 import pybullet as p
 import pybullet_data
+import numpy as np
 import time
 import os
 
 home_directory = os.getcwd()
-KR16_model = os.path.join(home_directory, "kuka_models/kuka_experimental-melodic-devel/kuka_kr16_support/urdf", "kr16_2.urdf")
+KR16_model = os.path.join(home_directory, "../kuka_models/kuka_experimental-melodic-devel/kuka_kr16_support/urdf", "kr16_2.urdf")
 
 def get_joint_info(robot):
     joints = p.getNumJoints(robot) - 2
@@ -29,7 +30,7 @@ def get_joint_variables(robot):
 
 def get_joint_velocities(robot):
     joints = p.getNumJoints(robot) - 2
-    return [j[1] for j in p.getJointStates(robot, range(joint))]
+    return [j[1] for j in p.getJointStates(robot, range(joints))]
 
 def get_poses(robot):
     position = []
@@ -40,6 +41,36 @@ def get_poses(robot):
         position.append(world_position)
         orientation.append(world_orientation)
     return position, orientation
+
+def skew(w):
+    return np.array([[0, -w[2], w[1]], [w[2], 0, -w[0]], [-w[1], w[0], 0]])
+
+def twist2ht(S, theta):
+    w = S[:, :3].T
+    w_ss = skew(w)
+
+    R = np.eye(3) + np.sin(theta) * w_ss + (1 - np.cos(theta)) * (w_ss @ w_ss)
+
+    TopRight = (np.eye(3) * theta + (1 - np.cos(theta)) * w_ss + (theta - np.sin(theta)) * (w_ss @ w_ss))
+
+    v = S[:, 3:].T
+
+    T = np.vstack((np.hstack((R, TopRight @ v)), [0, 0, 0, 1]))
+
+    return T
+
+def axisangle2rot(omega, theta):
+    omega_ss = skew(omega)
+    R = np.eye(3) + np.sin(theta) * omega_ss + (1 - np.cos(theta)) * (omega_ss @ w_ss)
+    return R
+
+def fkine_space(S, M, q):
+    S_ss = np.zeros(4,4*S.shape[1])
+    for i in range(S.shape[1]):
+        Si_ss = twist2ht(S[:, i], q[i])
+        S_ss[:, 4*i-3:4*i] = Si_ss
+
+
 
 physicsClient = p.connect(p.GUI) 
 p.setAdditionalSearchPath(pybullet_data.getDataPath())  
@@ -53,7 +84,7 @@ p.setGravity(0, 0, -9.81)
 p.setTimeStep(0.0001) 
 p.setRealTimeSimulation(0)
 
-num_joints = p.getNumJoints(robot_left) - 2      #two fixed joints are usedd in urdf
+num_joints = p.getNumJoints(robot_left) - 2      #two fixed joints are used in urdf
 
 #Spawning at initial configurations
 p.setJointMotorControlArray(
