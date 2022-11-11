7592a019cc9847634b9967e5f2a335df652b535c
DESKTOP-2U1VS39\malte <maltericklefs@web.de>
2022-11-11 17:34:54 -0500

    added path_line function to test inverse kinematics

--- src/Simulation_KR16.py
+++ src/Simulation_KR16.py
@@ -4,8 +4,8 @@ import numpy as np
 import time
 import os
 
-home_directory = os.getcwd()
-KR16_model = os.path.join(home_directory, "../kuka_models/kuka_experimental-melodic-devel/kuka_kr16_support/urdf", "kr16_2.urdf")
+# home_directory = os.getcwd()
+KR16_model =r"C:\Users\malte\Desktop\Project_Dynamics\RBE501-Dynamics\kuka_models\kuka_experimental-melodic-devel\kuka_kr16_support\urdf\kr16_2.urdf"
 
 def get_joint_info(robot):
     joints = p.getNumJoints(robot) - 2
@@ -159,6 +159,22 @@ def MatrixLog6(T):
 
     return exp_mat
 
+def path_line(start, goal):
+    # start [x, y, z]
+    # goal [x, y, z]
+    N = 100
+    path=[]
+    distance_x=(goal[0]-start[0])
+    distance_y=(goal[1]-start[1])
+    distance_z=(goal[2]-start[2])
+    for i in range(N+1):
+        dX=start[0]+i/N*distance_x
+        dY=start[1]+i/N*distance_y
+        dZ=start[2]+i/N*distance_z
+        path.append([start[0]+dX, start[1]+dY, start[2]+dZ])
+
+    return path
+
 
 theta = .57
 s = np.array([[1, 2, 3, 4, 5, 6]])
@@ -182,12 +198,16 @@ num_joints = p.getNumJoints(robot_left) - 2      #two fixed joints are used in u
 #Spawning at initial configurations
 p.setJointMotorControlArray(
 robot_left, range(num_joints), p.POSITION_CONTROL,
-targetPositions=[-0.5,0,0,0,0,0], positionGains = [0.01,0,0,0,0,0])
+targetPositions=[0,0,0,0,0,0], positionGains = [0.01,0,0,0,0,0])
 
 p.setJointMotorControlArray(
 robot_right, range(num_joints), p.POSITION_CONTROL,
-targetPositions=[0.5,0,0,0,0,0], positionGains = [0.01,0,0,0,0,0])
+targetPositions=[0,0,0,0,0,0], positionGains = [0.01,0,0,0,0,0])
 time.sleep(1. / 240.)
+
+start = np.array([0, 0, 0])
+goal = np.array([3, 2, 1])
+path = path_line(start, goal)
 
 for i in range(10000):
     p.stepSimulation()
@@ -198,8 +218,8 @@ for i in range(10000):
     '''
 
     #hard-coded joint variables
-    q_left = [-0.5,0,0,0,2,0]
-    q_right = [0.5,0,0,0,2,0]
+    q_left = [0,0,0,0,0,0]
+    q_right = [0,0,0,0,0,0]
 
     #Implementing position controllers
     p.setJointMotorControlArray(
