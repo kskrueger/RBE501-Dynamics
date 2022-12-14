29d9fd57b1ca02a6780481e166259d1cd593a193
ktrivedi <krutarth.ambarish.trivedi@gmail.com>
2022-12-13 20:33:32 -0500

    Added More UR5 Models

--- src/UR5_pybullet_IK.py
+++ src/UR5_pybullet_IK.py
@@ -8,9 +8,9 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, -9.8)
 planeId = p.loadURDF("plane.urdf")
 p.setRealTimeSimulation(1)
-#UR5 = p.loadURDF("/home/kt/Academics/RBE501/RBE501-Dynamics/UR5/urdf/ur5.urdf") # Krutarth
-UR5 = p.loadURDF(r"E:\GitHub\RBE501-Dynamics\UR5\urdf\ur5.urdf") # Kyle
-CUBE_model = "meshes/small_block.urdf"
+UR5 = p.loadURDF("/home/kt/Academics/RBE501/RBE501-Dynamics/UR5/urdf/UR5.urdf") # Krutarth
+# UR5 = p.loadURDF(r"E:\GitHub\RBE501-Dynamics\UR5\urdf\ur5.urdf") # Kyle
+CUBE_model = "/home/kt/Academics/RBE501/RBE501-Dynamics/src/meshes/small_block.urdf"      
 planeId = p.loadURDF("plane.urdf")
 cube_pos = [.5, 0, 0]
 cubeId = p.loadURDF(CUBE_model, cube_pos)
@@ -27,9 +27,12 @@ for joint_index in range(joints):
     world_position, world_orientation = p.getLinkState(UR5, joint_index)[:2]
     position.append(world_position)
     orientation.append(world_orientation)
-print(position[6], orientation[6])
+print(position[6], p.getMatrixFromQuaternion(orientation[6]))
 
-time.sleep(3)
+# time.sleep(3)
+# M = np.array([-1,0,0,0.805659],[0,-1,0,-0.110433],[0,0,1,0.354950])
+while True:
+    p.stepSimulation()
 
 #Go to the other location.
 p.setJointMotorControlArray(
@@ -51,6 +54,7 @@ damping[idx] = 0.1
 p.setJointMotorControlArray(
     UR5, range(len(Q)), p.POSITION_CONTROL,
     targetPositions=Q, positionGains=damping)
+
 
 time.sleep(100)
 
